f62a69a67b1fefd8fa307bd535dc037c88c9b13d
BuildTools <unconfigured@null.spigotmc.org>
2021-02-17 22:04:39 -0800

    Added main landing page with buttons and connected methods to switch activities

--- app/src/main/java/com/example/tvstats/MainActivity.java
+++ app/src/main/java/com/example/tvstats/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.tvstats;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    // List of background images
+    ArrayList<Integer> backgroundImages = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        backgroundImages.add(R.drawable.basic);
+    }
+
+    public void gotoAdd(View view) {
+    }
+    public void gotoMyShows(View view) {
+    }
+    public void gotoStats(View view) {
+    }
+    public void gotoSettings(View view) {
     }
 }
