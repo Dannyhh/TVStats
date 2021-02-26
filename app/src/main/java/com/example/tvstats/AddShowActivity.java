8b3a19c666206c0846fdb317bfec678185bfcaed
BuildTools <unconfigured@null.spigotmc.org>
2021-02-26 12:02:49 -0800

    Added base layout for adding a show and put skeleton functions in place

--- app/src/main/java/com/example/tvstats/AddShowActivity.java
+++ app/src/main/java/com/example/tvstats/AddShowActivity.java
@@ -1,7 +1,9 @@
 package com.example.tvstats;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddShowActivity extends Activity {
 
@@ -10,4 +12,11 @@ public class AddShowActivity extends Activity {
         setContentView(R.layout.activity_add_show);
     }
     //TODO: Add the layout and any functions for this class
+    public void addShow(View view) {
+        gotoMain(view);
+    }
+    public void gotoMain(View view) {
+        Intent myIntent = new Intent(view.getContext(), MainActivity.class);
+        startActivityForResult(myIntent, 0);
+    }
 }
