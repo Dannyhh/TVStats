a8391c1a583c2ca4e850c84f7760eb1bb6db038c
Dannyhh <dannyhh1k@gmail.com>
2021-02-28 18:32:21 -0800

    Added drop downs for rating and status, added error checking for integer values, and set the date automatically if the show is "completed"

--- app/src/main/java/com/example/tvstats/AddShowActivity.java
+++ app/src/main/java/com/example/tvstats/AddShowActivity.java
@@ -1,11 +1,18 @@
 package com.example.tvstats;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AddShowActivity extends Activity {
 
@@ -14,40 +21,72 @@ public class AddShowActivity extends Activity {
         setContentView(R.layout.activity_add_show);
     }
 
-    //TODO: add checking for null to throw popup, or throw popup on int parse fail
+
     public void addShow(View view) {
         //get the name from name field
         EditText nameEdit = findViewById(R.id.NameText);
         String title = nameEdit.getText().toString();
 
-        //get the total episodes from total field
-        EditText totalEp = findViewById(R.id.TotalEpisodes);
-        int total = Integer.parseInt(totalEp.getText().toString());
-
-        //get the watched count from watched field
-        EditText watchedEp = findViewById(R.id.CurrentEpisodes);
-        int watched = Integer.parseInt(watchedEp.getText().toString());
-
-        //get the rating from rating field
-        EditText ratingField = findViewById(R.id.Rating);
-        int rating = Integer.parseInt(ratingField.getText().toString());
+        //if there is no title throw an error and return
+        if(title.length() == 0){
+            exceptionToast(view.getContext(),
+                    "Must input a title for your show");
+            return;
+        }
 
         //get the status of the show
-        EditText statusField = findViewById(R.id.Status);
-        String status = ratingField.getText().toString();
+        Spinner statusField = findViewById(R.id.status_spinner);
+        String status = statusField.getSelectedItem().toString();
+        String date = "";
+        if(status.equals("Completed")) {
+            SimpleDateFormat sdf = new SimpleDateFormat("MM.dd.yyyy");
+            date = sdf.format(new Date());
+        }
 
+        try {
+            //get the total episodes from total field
+            EditText totalEp = findViewById(R.id.TotalEpisodes);
+            int total = Integer.parseInt(totalEp.getText().toString());
+            if(total < 0){
+                exceptionToast(view.getContext(),
+                        "Must have a positive number of total episodes");
+                return;
+            }
 
-        //get the date from date field
-        EditText dateField = findViewById(R.id.DateCompleted);
-        String date = dateField.getText().toString();
+            //get the watched count from watched field
+            EditText watchedEp = findViewById(R.id.CurrentEpisodes);
+            int watched = Integer.parseInt(watchedEp.getText().toString());
+            if(watched > total){
+                exceptionToast(view.getContext(),
+                        "Watched episodes must be less than total episodes");
+                return;
+            }
+            else if(watched < 0){
+                exceptionToast(view.getContext(),
+                        "Must have a positive number of watched episodes");
+                return;
+            }
 
+            //get the rating from rating field
+            Spinner ratingField = findViewById(R.id.rating_spinner);
+            int rating = Integer.parseInt(ratingField.getSelectedItem().toString());
 
-        MainActivity.dbh.addShow(title, total, watched, rating, status, date);
-        gotoMain();
+            MainActivity.dbh.addShow(title, total, watched, rating, status, date);
+            gotoMain();
+        }
+        catch (Exception e){
+            exceptionToast(view.getContext(),
+                    "Must input a number for total or watched episodes");
+        }
+
     }
     public void gotoMain(){
         Intent intent = new Intent(AddShowActivity.this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
+    }
+
+    public static void exceptionToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
 }
